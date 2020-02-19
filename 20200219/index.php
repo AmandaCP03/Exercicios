e61c60cba0a20004f4d2aa174ea224d57c845777
AmandaCP03 <acampezan@gmail.com>
2020-02-19 16:40:55 -0300

    links de exercicios

---
+++ 20200219/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Link</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Página com links dos meus exercícios</h1>
+</body>
+</html>
