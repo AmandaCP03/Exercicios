2a8a2d323cd380398e8752b4bf36ef7e5d7ebe90
AmandaCP03 <acampezan@gmail.com>
2020-02-19 16:03:40 -0300

    Exercicio nova versao3 20200219

--- 20200219/index.php
+++ 20200219/index.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8" />
     <title>Page Title</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="main.js"></script>
 </head>
 <body>
-<div class="container">
     <?php
         include "inc/menu.inc";
     ?>
@@ -43,7 +43,6 @@
         <div class = "col-4 offset-4">
             <button type="button" class="btn btn-outline-dark"> Agendar </button>
         </div>
-    </div>
     </div>
 </body>
 </html>
--- 20200219/recebe_agendamento.php
+++ 20200219/recebe_agendamento.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <?php
